cb66dc9814fd82b060058699537e4779d0f1cb9d
raiders032 <nameks@naver.com>
2020-06-25 16:29:53 +0900

    Initial commit

---
+++ src/test/java/com/youngthree/querydslstudy/QuerydslStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngthree.querydslstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
